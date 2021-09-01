c51756232f7b0a7bcc265785b9921e4948afafdb
JurinKI <Jukrin.sa@mail.wu.ac.th>
2021-09-01 15:24:13 +0700

    Add file

--- src/CalculateGrademedo.java
+++ src/CalculateGrademedo.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class CalculateGrademedo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input Score ");
+        int score = in.nextInt();
+        CalculateGradeTest c = new CalculateGradeTest(score);
+        System.out.println("grade "+c.calcuate());
+    }
 }
