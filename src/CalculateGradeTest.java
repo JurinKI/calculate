631c3ac93570873197b5832433f5e0f6303cf1e9
games1753 <natakorn175@gmail.com>
2021-09-01 15:16:11 +0700

    edit calculategradetest

--- src/CalculateGradeTest.java
+++ src/CalculateGradeTest.java
@@ -1,4 +1,21 @@
 
 public class CalculateGradeTest {
-
-}
+		private int score; 
+		
+		public CalculateGradeTest(int score) {
+			
+		}
+		
+		public int getScore() {
+			return 0; 
+			
+		}
+		
+		public void setScore(int score) 
+		
+		{ 
+		
+		} public char calcuate() {
+			return 'A'; 
+		} 
+	}
